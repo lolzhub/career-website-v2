708d461e80c1189d780bf86126745698034c85e4
anon <125595392+lolzhub@users.noreply.github.com>
2023-03-07 06:50:22 +0000

    added dynamic data and api routes + jinja templates

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 # print("Hello world")
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Pune, India',
+    'salary':1000
+  },
+    {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi, India',
+    'salary':2000
+  },
+    {
+    'id':3,
+    'title':'Frontend dev',
+    'location':'Remote',
+    'salary':3000
+  },
+    {
+    'id':4,
+    'title':'Backend dev',
+    'location':'Nagpur, India',
+    'salary':4000
+  }
+]
+
 @app.route("/")
 def helloWorld():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
